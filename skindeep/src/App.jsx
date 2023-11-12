237d7ea108c73398b3291f7fdc25ad9a3e826500
justineclaire <langridge.justine@gmail.com>
2023-11-12 13:38:52 +0000

    mysql connected

--- skindeep/src/App.jsx
+++ skindeep/src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import React from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([])
+
+  useEffect(()=> {
+    fetch('http://localhost:8081/skinIssues')
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(err => console.log(err));
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>issue</th>
+        </thead>
+        <tbody>
+          {data.map((d, i) => (
+            <tr key={i}>
+              <td>{d.issueID}</td>
+              <td>{d.issue}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
-export default App
+export default App;
